2906894148d4f8a70ae77f24c4bb2a69a007f349
Thamer Saraei <tsarei@legroupejsv.onmicrosoft.com>
2021-04-08 23:19:35 -0400

    Add update_mini_batch function

--- neural_nets.py
+++ neural_nets.py
@@ -29,6 +29,23 @@ class Network (object):
 
         return a
     
-    
+    def SGD(self, training_data, epochs, mini_batch_size, eta, 
+                            test_data = None):
+
+        if test_data:
+            n_test = len(test_data)
+        n = len(training_data)
+
+        for j in range(epochs):
+            random.shuffle(training_data)
+            mini_batches = [
+                training_data[k:k+mini_batch_size] for k in range(0, n, mini_batch_size)
+            ]
+        
+        for mini_batch in mini_batches:
+            self.update_mini_batch(mini_batch, eta)
+
+        if test_data:
+            print('Epoch ')
 
 
